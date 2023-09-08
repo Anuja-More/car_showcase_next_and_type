31a27c9c3d3ed9aa11191ece09ecf0217304df8a
Anuja-More-HFN <anuja.more@volunteer.heartfulness.org>
2023-09-08 17:40:31 +0530

    card ui change

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -21,9 +21,39 @@ const Card = ({ carData }: carProps) => {
       <h2 className="text-xl font-semibold mt-2">
         {carData.make} {carData.model}
       </h2>
+
       <p className="text-gray-600">Year: {carData.year}</p>
-      <p className="text-gray-600">City MPG: {carData.city_mpg}</p>
-      <p className="text-gray-600">Highway MPG: {carData.highway_mpg}</p>
+      <p className="text-gray-600">No. of Owners: {carData.no_of_owners}</p>
+      <p className="text-gray-600">Kilometers Driven: {carData.km_driven}</p>
+      <p className="text-gray-600">Location: pune</p>
+      <p className="text-gray-600">Date of post: 20/08/2023</p>
+      <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                 <Image src="/steering-wheel.svg" width={20} height={20} 
+                 alt="steering wheel" />
+                 <p className='text-[14px]'>
+                 {carData.transmission}
+                 </p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                 <Image src="/tire.svg" width={20} height={20} 
+                 alt="tire" />
+                 <p className='text-[14px]'>
+                    {carData.year}
+                 </p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                 <Image src="/gas.svg" width={20} height={20} 
+                 alt="steering wheel" />
+                 <p className='text-[14px]'>
+                 {carData.fuel_type}
+                 </p>
+                </div>
+            </div>
+            <div className="car-card__btn-container">
+        </div>
+        </div>
       <div className="flex justify-between mt-4">
         <Link href={`/edit_cars/${carData._id}`} className="text-blue-500 hover:underline">
           Edit
